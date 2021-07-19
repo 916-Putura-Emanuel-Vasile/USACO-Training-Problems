5c588f76557db35722c2d70c63274aa7202ac128
916-Putura-Emanuel-Vasile <72196173+916-Putura-Emanuel-Vasile@users.noreply.github.com>
2021-07-19 12:18:34 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Problems.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problems.dir/beads.cpp.obj"
+  "Problems.exe"
+  "Problems.exe.manifest"
+  "Problems.lib"
+  "Problems.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problems.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
